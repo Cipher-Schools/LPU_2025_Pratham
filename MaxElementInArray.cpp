96170f7dbf41e59f0560406e1182ee5cc00d8b13
Pratham-a02 <prathampahad012@gmail.com>
2025-10-30 19:56:57 +0530

    Create MaxElementInArray.cpp

---
+++ MaxElementInArray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int findMax(int arr[], int n) {
+    int maxVal = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > maxVal)
+            maxVal = arr[i];
+    }
+    return maxVal;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    cout << "Maximum = " << findMax(arr, n);
+}
