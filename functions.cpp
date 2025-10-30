4d74986fa3c12202b0ecd2904455a5058309817c
Pratham-a02 <prathampahad012@gmail.com>
2025-10-30 19:34:38 +0530

    Create functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello!" << endl;
+    cout << "Hello!" << endl;
+    cout << "Hello!" << endl;
+}
+
+*****************************************************
+
+#include <iostream>
+using namespace std;
+
+void greet() {
+    cout << "Hello!" << endl;
+}
+
+int main() {
+    greet();
+    greet();
+    greet();
+}
