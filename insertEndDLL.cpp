90f61c695ebedf1a2f7db5c6d698fe2d0a72c651
Pratham-a02 <prathampahad012@gmail.com>
2025-11-05 09:45:14 +0530

    Create insertEndDLL.cpp

---
+++ insertEndDLL.cpp
@@ -0,0 +1,12 @@
+void insertAtTail(Node*& head, int data) {
+    Node* newNode = new Node(data);
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = newNode;
+    newNode->prev = temp;
+}
